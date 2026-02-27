25ebc9e1357d71ce00b29439caa58983a4747adb
Isaac Olson <isaac.olson@skyspecs.com>
2026-02-26 20:56:40 -0500

    ergogen script

---
+++ ergogen/gen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#### Find the script directory regardless of symlinks, etc
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    # if $SOURCE was a relative symlink, we need to resolve it relative
+    # to the path where the symlink file was located
+done
+THISDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+####
+
+cd $THISDIR
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm use 22
+
+ergogen --clean --svg .
+
